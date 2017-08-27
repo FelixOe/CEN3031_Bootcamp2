eb0d75431006ed3d801714d9b8f5d65f1c1201b5
FelixOe <felix90@ufl.edu>
2017-08-27 18:33:25 +0200

    Finished assignment 2

--- listingController.js
+++ listingController.js
@@ -1,14 +1,53 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
-  function($scope, Listings) {
-    $scope.listings = Listings;
-    $scope.detailedInfo = undefined;
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
+    function ($scope, Listings) {
+        $scope.listings = Listings;
+        $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
-  }
+        //Model for Search
+        $scope.searchTerm = '';
+
+        //Model for new listings
+        $scope.addCode = '';
+        $scope.addName = '';
+        $scope.addLatitude = '';
+        $scope.addLongitude = '';
+        $scope.addAddress = '';
+        $scope.addCity = '';
+        $scope.addState = '';
+        $scope.addZip = '';
+        $scope.addCountry = '';
+
+
+        $scope.addListing = function () {
+            var newlisting = {
+                code: $scope.addCode,
+                name: $scope.addName,
+                coordinates: {
+                    latitude: $scope.addLatitude,
+                    longitude: $scope.addLongitude
+                },
+                address: $scope.addAddress + ', ' + $scope.addCity + ', ' + $scope.addState + ' ' + $scope.addZip + ', ' + $scope.addCountry
+            };
+            $scope.listings.splice(0, 0, newlisting);
+            resetModel();
+        };
+        $scope.deleteListing = function (index) {
+            $scope.detailedInfo = undefined;
+            $scope.listings.splice(index, 1);
+        };
+        $scope.showDetails = function (index) {
+            $scope.detailedInfo = Listings[index];
+        };
+        resetModel = function () {
+            $scope.addCode = '';
+            $scope.addName = '';
+            $scope.addLatitude = '';
+            $scope.addLongitude = '';
+            $scope.addAddress = '';
+            $scope.addCity = '';
+            $scope.addState = '';
+            $scope.addZip = '';
+            $scope.addCountry = '';
+        }
+    }
 ]);
